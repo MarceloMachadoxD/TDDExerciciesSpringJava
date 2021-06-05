d9908193ad623361260fa43dd0c3511fc72e33dd
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-05 17:49:49 -0300

    Employee insert

--- src/main/java/com/devsuperior/bds01/services/EmployeeService.java
+++ src/main/java/com/devsuperior/bds01/services/EmployeeService.java
@@ -1,14 +1,21 @@
 package com.devsuperior.bds01.services;
 
 import com.devsuperior.bds01.dto.EmployeeDTO;
+import com.devsuperior.bds01.entities.Department;
 import com.devsuperior.bds01.entities.Employee;
 import com.devsuperior.bds01.repositories.DepartmentRepository;
 import com.devsuperior.bds01.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,10 +25,20 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Transactional(readOnly = true)
     public Page<EmployeeDTO> findAll(Pageable page){
         Page<Employee> list = employeeRepository.findAll(page);
 
         return list.map(x -> new EmployeeDTO(x));
     }
 
+    @Transactional(readOnly = false)
+    public EmployeeDTO insert(EmployeeDTO dto) {
+        Employee employee = new Employee();
+        employee.setName(dto.getName());
+        employee.setEmail(dto.getEmail());
+        employee.setDepartment(new Department(dto.getDepartmentId(), null));
+        employee = employeeRepository.save(employee);
+        return new EmployeeDTO(employee);
+    }
 }
